23458939d6940bf7cc91537ceb1157c5035d3996
SenseiBobo <bogdanjsd@gmail.com>
2021-12-28 06:02:24 +0100

    added modes for camera

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -4,5 +4,4 @@ func _process(delta):
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 	if Input.is_action_pressed("restart"):
-		var scene = get_tree().get_current_scene()
-		get_tree().change_scene(scene.get_filename())
+		get_tree().reload_current_scene()
--- Scenes/PlayerCamera/PlayerCamera.gd
+++ Scenes/PlayerCamera/PlayerCamera.gd
@@ -1,19 +1,42 @@
 extends Camera2D
 
-export var path_to_player : NodePath
-export var path_to_camera_path : NodePath
+enum MODE {
+	FREE,	# no paths to follow and no areas to stay in
+	
+	PATH,	# follows the Curve2D that's defined in the specified Path2D ndoe
+			# use "path_to_camera_path"
+			# utilizes Curve2D's get_closest_point() method
+	
+	AREA,	# stays in area that's defined in the specified Navigation2D node
+			# use "path_to_camera_area"
+			# utilizes Navigation2D's get_closest_point() method
+			
+	POINT	# looks at a specific point defined in the camera_point variable
+}
+
+
+export(MODE) var mode : int = MODE.PATH
+export var path_to_player : NodePath # required
+export var path_to_camera_path : NodePath # only required for path mode
+export var path_to_camera_area : NodePath # only required for area mode
+export var camera_point : Vector2
 export var smoothing : bool = true
 export var camera_speed : float = 8
 
 onready var player = get_node(path_to_player)
 onready var camera_path : Path2D = get_node_or_null(path_to_camera_path)
+onready var camera_area : Navigation2D = get_node_or_null(path_to_camera_area)
 
 func _physics_process(delta):
 	var new_position : Vector2
-	if is_instance_valid(camera_path): # if camera_path has been provided
-		var curve : Curve2D = camera_path.curve
-		new_position = curve.get_closest_point(player.global_position)
-	else: # otherwise, camera_path is null
+	
+	if mode == MODE.PATH and is_instance_valid(camera_path):
+		new_position = camera_path.curve.get_closest_point(player.global_position)
+	elif mode == MODE.AREA and is_instance_valid(camera_area):
+		new_position = camera_area.get_closest_point(player.global_position)
+	elif mode == MODE.POINT:
+		new_position = camera_point
+	else:
 		new_position = player.global_position
 		
 	
